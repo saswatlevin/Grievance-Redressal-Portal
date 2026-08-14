85833928648561ffd1e3f1e677ff8d4c00a83270
Saswat K. Levin <saswatlevin@gmail.com>
2026-08-15 00:29:10 +0530

    Updated the app.module.ts file with the ChainsModule

--- grievance_redressal_portal/src/app.module.ts
+++ grievance_redressal_portal/src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { DatabaseModule } from './database/database.module';
+import { ChainsModule } from './chains/chains.module';
 
 @Module({
-  imports: [UsersModule, DatabaseModule],
+  imports: [UsersModule, DatabaseModule, ChainsModule],
   controllers: [AppController],
   providers: [AppService],
 })
