f7d25f3bec56a3d18fc3f57a81da5e7e9a843ea1
Saswat K. Levin <saswatlevin@gmail.com>
2026-08-15 15:42:07 +0530

    Extended the updateChainAddressDto with PartialType to allow some address fields to be updated

--- grievance_redressal_portal/src/chains/dto/update-chain-address.dto.ts
+++ grievance_redressal_portal/src/chains/dto/update-chain-address.dto.ts
@@ -1,7 +1,8 @@
-import { OmitType } from '@nestjs/swagger';
+import { PartialType, OmitType } from '@nestjs/swagger';
 import { CreateChainDto } from './create-chain.dto';
 
-export class UpdateChainAddressDto extends OmitType(
-  CreateChainDto,
-  ['chain_name'] as const,
+// Using PartialType allows only some fields to be updated.
+// Using OmitType allows the chain_name field to be omitted.
+export class UpdateChainAddressDto extends PartialType(
+  OmitType(CreateChainDto, ['chain_name'] as const),
 ) {}
