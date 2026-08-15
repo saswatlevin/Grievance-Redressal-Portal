e970e329833bb3921e6ba6e73eec8f11c417d138
Saswat K. Levin <saswatlevin@gmail.com>
2026-08-15 15:43:17 +0530

    Added validators to updateChainNameDto

--- grievance_redressal_portal/src/chains/dto/update-chain-name.dto.ts
+++ grievance_redressal_portal/src/chains/dto/update-chain-name.dto.ts
@@ -1,6 +1,10 @@
 import { ApiProperty } from '@nestjs/swagger';
+import {IsString, IsNotEmpty, MaxLength} from 'class-validator';
 
 export class UpdateChainNameDto {
   @ApiProperty()
+  @IsString()
+  @IsNotEmpty()
+  @MaxLength(100)
   chain_name!: string;
 }
