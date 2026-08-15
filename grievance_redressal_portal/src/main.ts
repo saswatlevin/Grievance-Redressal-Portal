092de9b6e16c6f8980e99212443c7f85d425fb16
Saswat K. Levin <saswatlevin@gmail.com>
2026-08-15 11:43:07 +0530

    Implemented global bigint serialization in main.ts

--- grievance_redressal_portal/src/main.ts
+++ grievance_redressal_portal/src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+// Implements BigInt serialization
+// That is, allows the conversion of BigInt to JSON in the response.
+(BigInt.prototype as any).toJSON = function () {
+  return this.toString();
+};
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT ?? 3000);
 }
+
 bootstrap();
