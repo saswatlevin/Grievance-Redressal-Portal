daa58efceb40e7e850e8d8ad967884d656fc8c4a
Saswat K. Levin <saswatlevin@gmail.com>
2026-08-17 15:57:19 +0530

    Added the PrismaModule to imports in chains.module.ts.

--- grievance_redressal_portal/src/chains/chains.module.ts
+++ grievance_redressal_portal/src/chains/chains.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ChainsService } from './chains.service';
 import { ChainsController } from './chains.controller';
+import { PrismaModule } from '../prisma/prisma.module';
 
 @Module({
   controllers: [ChainsController],
   providers: [ChainsService],
+  imports: [PrismaModule]
 })
 export class ChainsModule {}
