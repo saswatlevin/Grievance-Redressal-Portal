2e87e794782592cd9fc144c75e4ce378e13fe33d
Saswat K. Levin <saswatlevin@gmail.com>
2026-08-17 15:56:04 +0530

    Replaced @IsString() with @IsInt() in update-chain-name.dto.ts.

--- grievance_redressal_portal/src/chains/dto/update-chain-name.dto.ts
+++ grievance_redressal_portal/src/chains/dto/update-chain-name.dto.ts
@@ -1,10 +1,12 @@
 import { ApiProperty } from '@nestjs/swagger';
-import {IsString, IsNotEmpty, MaxLength} from 'class-validator';
+import {IsString, IsNotEmpty, MaxLength, Matches} from 'class-validator';
+import {CHAIN_NAME_REGEX, CHAIN_NAME_MAXIMUM_LENGTH} from '../chains.constants';
 
 export class UpdateChainNameDto {
   @ApiProperty()
   @IsString()
   @IsNotEmpty()
-  @MaxLength(100)
+  @MaxLength(CHAIN_NAME_MAXIMUM_LENGTH)
+  @Matches(CHAIN_NAME_REGEX)
   chain_name!: string;
 }
