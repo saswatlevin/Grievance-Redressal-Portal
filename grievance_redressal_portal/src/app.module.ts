18e7d3c1a1d8c0caf2afc331a20351770da5773c
Saswat K. Levin <saswatlevin@gmail.com>
2026-08-17 15:41:37 +0530

    Add the OutletsModule to imports in app.module.ts

--- grievance_redressal_portal/src/app.module.ts
+++ grievance_redressal_portal/src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { ChainsModule } from './chains/chains.module';
+import { OutletsModule } from './outlets/outlets.module';
 
 @Module({
-  imports: [UsersModule, PrismaModule, ChainsModule],
+  imports: [UsersModule, PrismaModule, ChainsModule, OutletsModule],
   controllers: [AppController],
   providers: [AppService],
 })
