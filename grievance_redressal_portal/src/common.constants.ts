ebf1fa7d11adbbd29aaef6de059b5868672a7738
Saswat K. Levin <saswatlevin@gmail.com>
2026-08-17 15:42:53 +0530

    Created common constants for regex and maximum length of address fields

---
+++ grievance_redressal_portal/src/common.constants.ts
@@ -0,0 +1,21 @@
+export const ADDRESS_ROOM_NUMBER_REGEX = /^[0-9]*$/;
+export const ADDRESS_ROOM_NUMBER_MAXIMUM_LENGTH = 5;
+
+export const ADDRESS_BUILDING_REGEX = /^[A-Za-z0-9\-.' ]*$/;
+export const ADDRESS_BUILDING_MAXIMUM_LENGTH = 50;
+
+export const ADDRESS_STREET_REGEX = /^[A-Za-z0-9\-.' ]*$/;
+export const ADDRESS_STREET_MAXIMUM_LENGTH = 100;
+
+export const ADDRESS_CITY_REGEX = /^[A-Za-z\-.' ]*$/;
+export const ADDRESS_CITY_MAXIMUM_LENGTH = 100;
+
+export const ADDRESS_ADMIN_DIVISION_REGEX = /^[A-Za-z\-.' ]*$/;
+export const ADDRESS_ADMIN_DIVISION_MAXIMUM_LENGTH = 100;
+
+export const ADDRESS_COUNTRY_REGEX = /^[A-Za-z\- ]*$/;
+export const ADDRESS_COUNTRY_MAXIMUM_LENGTH = 100;
+
+export const ADDRESS_POST_CODE_REGEX = /^[A-Za-z0-9\- ]*$/;
+export const ADDRESS_POST_CODE_MAXIMUM_LENGTH = 12;
+
