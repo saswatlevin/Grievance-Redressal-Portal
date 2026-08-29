2d8b48697af49361d3fdce5a1e768a7b2e2346ab
Saswat K. Levin <saswatlevin@gmail.com>
2026-08-29 13:03:37 +0530

    Kept the regex of the post_title field and an enum of the post_status field in posts.constants.ts.

---
+++ grievance_redressal_portal/src/posts/posts.constants.ts
@@ -0,0 +1,10 @@
+export const POST_TITLE_MAXiMUM_LENGTH = 2000;
+export const POST_CONTENT_MAXiMUM_LENGTH = 20000;
+
+export enum POST_STATUS {
+    OPEN = 'open',
+    LOCKED = 'locked',
+    RESOLVED = 'resolved',
+    SUSPENDED = 'suspended',
+    ARCHIVED = 'archived',
+};
