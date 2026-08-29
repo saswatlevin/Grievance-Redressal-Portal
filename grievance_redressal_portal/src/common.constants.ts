34c703a440beeffe45cc35ff45fded86eb89b0f3
Saswat K. Levin <saswatlevin@gmail.com>
2026-08-29 10:56:33 +0530

    Added the paragraph regex constant to common.constants.ts.

--- grievance_redressal_portal/src/common.constants.ts
+++ grievance_redressal_portal/src/common.constants.ts
@@ -18,3 +18,5 @@ export const ADDRESS_COUNTRY_MAXIMUM_LENGTH = 100;
 
 export const ADDRESS_POST_CODE_REGEX = /^[A-Za-z0-9\- ]*$/;
 export const ADDRESS_POST_CODE_MAXIMUM_LENGTH = 12;
+
+export const PARAGRAPH_REGEX = /^[A-Za-zÀ-ÖØ-öø-ÿ0-9#!,;:%'’“”\\\"\(\)\-\—\. ]*$/;
