a608f0aa4c361685524031ebdc88078c8e57d8ae
Saswat K. Levin <saswatlevin@gmail.com>
2026-08-29 13:02:23 +0530

    Created the UpdatePostTitleDto for the UpdatePostTitle controller and service.

---
+++ grievance_redressal_portal/src/posts/dto/update-post-title.dto.ts
@@ -0,0 +1,8 @@
+import { PartialType, OmitType } from '@nestjs/swagger';
+import { CreatePostDto } from './create-post.dto';
+
+// Using PartialType allows only some fields to be updated.
+// Using OmitType allows the necessary fields to be omitted.
+export class UpdatePostTitleDto extends PartialType(
+  OmitType(CreatePostDto, ['post_content', 'post_content', 'user_id'] as const),
+) {}
