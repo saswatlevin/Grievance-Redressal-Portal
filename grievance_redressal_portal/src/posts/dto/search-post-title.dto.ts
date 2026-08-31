148dbc0b9cce63378c0218bc90a10369459275bf
Saswat K. Levin <saswatlevin@gmail.com>
2026-08-31 11:04:02 +0530

    Corrected error in search post title dto.

--- grievance_redressal_portal/src/posts/dto/search-post-title.dto.ts
+++ grievance_redressal_portal/src/posts/dto/search-post-title.dto.ts
@@ -4,5 +4,5 @@ import { CreatePostDto } from './create-post.dto';
 // Using PartialType allows only some fields to be updated.
 // Using OmitType allows the necessary fields to be omitted.
 export class SearchPostTitleDto extends PartialType (
-    OmitType(CreatePostDto, ['post_content', 'post_content', 'user_id'] as const),
+    OmitType(CreatePostDto, ['post_content', 'post_status', 'user_id'] as const),
 ){}
