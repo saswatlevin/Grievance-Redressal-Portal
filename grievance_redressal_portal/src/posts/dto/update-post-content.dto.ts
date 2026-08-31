26816dcace2303b9f4da81bf4c0a930218962a93
Saswat K. Levin <saswatlevin@gmail.com>
2026-08-31 11:04:27 +0530

    Corrected logical error in ypdate-post-content.dto.ts.

--- grievance_redressal_portal/src/posts/dto/update-post-content.dto.ts
+++ grievance_redressal_portal/src/posts/dto/update-post-content.dto.ts
@@ -4,5 +4,5 @@ import { CreatePostDto } from './create-post.dto';
 // Using PartialType allows only some fields to be updated.
 // Using OmitType allows the necessary fields to be omitted.
 export class UpdatePostContentDto extends PartialType(
-  OmitType(CreatePostDto, ['post_title', 'post_content', 'user_id'] as const),
+  OmitType(CreatePostDto, ['post_title', 'post_status', 'user_id'] as const),
 ) {}
