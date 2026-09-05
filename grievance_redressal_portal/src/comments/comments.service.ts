51e11d36351b53409989a1728022a70a7d9b1fa8
Saswat K. Levin <saswatlevin@gmail.com>
2026-09-06 01:02:12 +0530

    Created the first draft of the comments service with the createComment, displayAllPostComments, searchCommentsByPost, updateCommentContent, softDeleteComment, softDeleteReply, hardDeleteComment, hardDeleteReply and searchCommentsByTitle service functions.

---
+++ grievance_redressal_portal/src/comments/comments.service.ts
@@ -0,0 +1,86 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
+import { CreateCommentDto } from './dto/create-comment.dto';
+import { UpdateCommentContentDto } from './dto/update-comment-content.dto';
+import { SoftDeleteCommentDto } from './dto/soft-delete-comment.dto';
+import { SoftDeleteReplyDto } from './dto/soft-delete-reply.dto';
+import { HardDeleteReplyDto } from './dto/hard-delete-reply.dto';
+import { SearchCommentsByPostDto } from './dto/search-comments-by-post.dto';
+import { CreateReplyDto } from './dto/create-reply.dto';
+
+@Injectable()
+export class CommentsService {
+    constructor (private readonly prisma: PrismaService) {}
+
+     async createComment(createCommentDto: CreateCommentDto) {
+        return this.prisma.comment.create({
+          data: createCommentDto
+        });
+      }
+
+     async createReply(createReplyDto: CreateReplyDto) {
+        ;
+     }
+    
+      async displayAllPostComments(postId: number) {
+        return this.prisma.$queryRaw`
+        SELECT comment_content, comment_is_deleted
+        FROM comments
+        WHERE comments.post_id = ${postId};
+      `;
+      }
+
+      async searchCommentsByPost(postId: number, searchCommentsByPostDto: SearchCommentsByPostDto) {
+        return this.prisma.$queryRaw`
+        SELECT comment_content, comment_is_deleted 
+        FROM comments 
+        WHERE post_id = ${postId} 
+        AND comment_content ILIKE ${searchCommentsByPostDto.comment_content + '%'};
+      `;
+      }
+    
+      async updateCommentContent(commentId: number, updateCommentContentDto: UpdateCommentContentDto) {
+         return this.prisma.comment.update({
+          where: {
+            comment_id: commentId,
+          },
+          data: updateCommentContentDto
+        })
+      }
+    
+      async softDeleteComment(commentId: number, softDeleteCommentDto: SoftDeleteCommentDto) {
+         return this.prisma.comment.update({
+          where: {
+            comment_id: commentId,
+          },
+          data: softDeleteCommentDto
+        })
+      }
+
+      async softDeleteReply(commentId: number, softDeleteReplyDto: SoftDeleteReplyDto) {
+         return this.prisma.comment.update({
+          where: {
+            comment_id: commentId,
+          },
+          data: softDeleteReplyDto
+        })
+      }
+    
+      async hardDeleteComment(commentId: number) {
+        return this.prisma.comment.delete({
+        where: {
+          comment_id: commentId,
+        },
+      });
+    }
+
+    async hardDeleteReply(commentId: number, hardDeleteReplyDto: HardDeleteReplyDto) {
+        return this.prisma.comment.delete({
+        where: {
+          comment_id: commentId,
+          reply_id: hardDeleteReplyDto.reply_id
+        },
+      })
+    }
+
+}
