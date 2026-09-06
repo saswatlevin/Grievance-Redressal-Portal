8733c8fc646ad0e86a90cdc43575c2c27d2a99ef
Saswat K. Levin <saswatlevin@gmail.com>
2026-09-07 00:47:52 +0530

    Deleted the soft delete reply DTO.

--- grievance_redressal_portal/src/comments/dto/soft-delete-reply.dto.ts
+++
@@ -1,8 +0,0 @@
-import { PickType } from '@nestjs/swagger';
-import { CommentUniversalDto } from './comment-universal.dto';
-
-// PickType helps us select those fields that we want to include in the class.
-export class SoftDeleteReplyDto extends PickType (
-   CommentUniversalDto,
-   ['comment_is_deleted'] as const,
-) {}
