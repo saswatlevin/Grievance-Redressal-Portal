698b1f3c5e6b1718bffd4049102923894bc9113f
Saswat K. Levin <saswatlevin@gmail.com>
2026-09-07 00:46:16 +0530

    Added  @Body to all those controllers that have a DTO.

--- grievance_redressal_portal/src/comments/comments.controller.ts
+++ grievance_redressal_portal/src/comments/comments.controller.ts
@@ -3,7 +3,6 @@ import { CommentsService } from './comments.service';
 import { CreateCommentDto } from './dto/create-comment.dto';
 import { UpdateCommentContentDto } from './dto/update-comment-content.dto';
 import { SoftDeleteCommentDto } from './dto/soft-delete-comment.dto';
-import { SoftDeleteReplyDto } from './dto/soft-delete-reply.dto';
 import { SearchCommentsByPostDto } from './dto/search-comments-by-post.dto';
 
 
@@ -24,13 +23,13 @@ export class CommentsController {
     }
 
     @Get('search_comments_by_post/:post_id')
-    searchCommentsByPost(@Param('post_id', ParseIntPipe) post_id: number, searchCommentsByPostDto: SearchCommentsByPostDto) {
+    searchCommentsByPost(@Param('post_id', ParseIntPipe) post_id: number, @Body() searchCommentsByPostDto: SearchCommentsByPostDto) {
         console.log("In searchCommentsByPost");
         return this.commentsService.searchCommentsByPost(post_id, searchCommentsByPostDto);
     }
 
     @Patch('soft_delete_comment/:comment_id')
-    softDeleteComment(@Param('comment_id', ParseIntPipe) comment_id: number, softDeleteCommentDto: SoftDeleteCommentDto) {
+    softDeleteComment(@Param('comment_id', ParseIntPipe) comment_id: number, @Body() softDeleteCommentDto: SoftDeleteCommentDto) {
         console.log("In softDeleteComment");
         return this.commentsService.softDeleteComment(comment_id, softDeleteCommentDto)
     }
@@ -41,21 +40,9 @@ export class CommentsController {
         return this.commentsService.hardDeleteComment(comment_id);
     }
 
-    @Patch('update_comment_content/:comment_id/:parent_comment_id')
-    updateCommentContent(@Param('comment_id', ParseIntPipe) comment_id: number, @Param('parent_comment_id', ParseIntPipe) parent_comment_id: number, updateCommentContentDto: UpdateCommentContentDto) {
+    @Patch('update_comment_content/:comment_id')
+    updateCommentContent(@Param('comment_id', ParseIntPipe) comment_id: number, @Body() updateCommentContentDto: UpdateCommentContentDto) {
         console.log("In updateCommentContent");
-        return this.commentsService.updateCommentContent(comment_id, parent_comment_id, updateCommentContentDto);
-    }
-
-    @Patch('soft_delete_reply/:comment_id/:parent_comment_id')
-    softDeleteReply(@Param('comment_id', ParseIntPipe) comment_id: number, @Param('parent_comment_id', ParseIntPipe) parent_comment_id: number, softDeleteReplyDto: SoftDeleteReplyDto) {
-        console.log("In softDeleteReply");
-        return this.commentsService.softDeleteReply(comment_id, parent_comment_id, softDeleteReplyDto);
-    }
-
-    @Delete('hard_delete_reply/:comment_id/:parent_comment_id')
-    hardDeleteReply(@Param('comment_id', ParseIntPipe) comment_id: number,  @Param('parent_comment_id', ParseIntPipe) parent_comment_id: number) {
-        console.log("In hardDeleteReply");
-        return this.commentsService.hardDeleteReply(comment_id, parent_comment_id);
+        return this.commentsService.updateCommentContent(comment_id, updateCommentContentDto);
     }
 }
